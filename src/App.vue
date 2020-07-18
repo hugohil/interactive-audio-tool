5a9019f4a571c70718e985903e368f3538082d01
hugohil <hilairehugo@gmail.com>
2020-07-18 18:52:14 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <h2>source</h2>
+      <select v-model="sourceID">
+        <option
+        v-for="source in sources"
+        :value="source.deviceId">
+          {{ source.label }}
+        </option>
+      </select>
+    </section>
+
+    <section>
+      <p>gain: {{ gain.toFixed(8) }}</p>
+      <p class="mark">&#10012;</p>
+      <p>pan: {{ pan.toFixed(8) }}</p>
+    </section>
+
+    <section>
+      <h2>mode</h2>
+      <select v-model="mode">
+        <option value="all">all</option>
+        <option value="pan">pan</option>
+        <option value="gain">gain</option>
+      </select>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as audio from '@/lib/audio'
+import debounce from 'lodash.debounce'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      mode: 'all',
+      pan: 0,
+      gain: .5,
+      sourceID: '',
+      sources: []
+    }
+  },
+  watch: {
+    sourceID (value) {
+      audio.connectUserMedia(value)
+    }
+  },
+  mounted () {
+    audio.getDeviceSources().then(sources => {
+      this.sources = sources
+    })
+
+    const mouseHandler = debounce(({ clientX, clientY }) => {
+      // if (audio.isActive()) {
+      if (true) {
+        this.gain = (1 - (clientY / window.innerHeight))
+        this.pan = (((clientX / window.innerWidth) - .5) * 2)
+
+        if (this.mode === 'all') {
+          audio.setGain(this.gain)
+          audio.setPanner(this.pan)
+        } else if (this.mode === 'gain') {
+          audio.setGain(this.gain)
+        } else if (this.mode === 'pan') {
+          audio.setPanner(this.pan)
+        }
+      }
+    }, 32, { leading: true, trailing: false })
+
+    document.body.addEventListener('mousemove', mouseHandler)
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body, html {
+  background: #0A0A02;
+  overflow: hidden;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  cursor: crosshair;
+}
+
+#app {
+  font-family: monospace;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  box-sizing: border-box;
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border: 1px solid #FFFFFF;
+}
+
+section {
+  color: #FFFFFF;
+  text-align: center;
+  border: 1px solid #FFFFFF;
+  min-width: 250px;
+  max-width: 400px;
+  padding: 2em 0;
+  margin: auto;
+
+  h2 {
+    margin: 1em 0 2em;
+  }
+
+  .mark {
+    font-size: 64px;
+  }
+}
+
+option, select {
+  cursor: pointer;
 }
 </style>
---
+++ src/lib/audio.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const context = new AudioContext()
+const gain = context.createGain()
+const panner = context.createStereoPanner()
+
+let active = false
+
+export function isActive () {
+  return active
+}
+
+export function getDeviceSources () {
+  return navigator.mediaDevices.enumerateDevices()
+    .then(devices => devices.filter(device => /audioinput/i.test(device.kind)))
+}
+
+export function setGain (value) {
+  gain.gain.value = value
+}
+
+export function setPanner (value) {
+  panner.pan.value = value
+}
+
+export function connectUserMedia (deviceID) {
+  navigator.mediaDevices.getUserMedia({
+    video: false,
+    audio: {
+      deviceId: {
+        exact: deviceID
+      }
+    }
+  }).then((stream) => {
+    const source = context.createMediaStreamSource(stream)
+
+    source.channelCountMode = 'explicit'
+    source.channelInterpretation = 'discrete'
+
+    source.connect(panner)
+    panner.connect(gain)
+    gain.connect(context.destination)
+
+    active = true
+  }).catch((err) => {
+    console.error(err)
+  })
+}
